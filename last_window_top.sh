6811a6995f854ca5c1f55b114c3e141e7f69faa3
James Pilcher <pilcherjames0@gmail.com>
2023-07-18 10:15:23 +0900

    updated it so it specifies 'Terminal' windows!

--- last_window_top.sh
+++ last_window_top.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# Get the ID of the last opened window
-window_id=$(wmctrl -l | tail -n 1 | awk '{print $1}')
+#!/bin/bash
 
+# Get the name of the user
+user_name=$USER
+
+# Get the ID of the last opened window that contains the user's name
+
+window_id=$(wmctrl -l | awk '$0 ~ /Terminal/ {id=$1} END{print id}')
+
+echo "$window_id"
 # Set the window to always be on top
 wmctrl -i -r "$window_id" -b add,above
+
